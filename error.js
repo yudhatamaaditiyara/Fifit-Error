2afb0479b274778e7c05b6203049c52b8140fec7
Yudha Tama Aditiyara <yudhatamaaditiyara@gmail.com>
2019-10-22 22:23:34 +0700

    .

---
+++ error.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2019 Yudha Tama Aditiyara
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const status = require("fifit-util-status");
+const RuntimeError = require("ganiyem-error/runtimeError");
+
+/**
+ */
+class Error extends RuntimeError
+{
+	/**
+	 * @param {number|void} status
+	 * @param {string|void} message
+	 * @param {Object|void} headers
+	 */
+	constructor(status, message, headers){
+		super(message);
+		Object.defineProperty(this, 'status', {
+			value: this._filterStatus(status), configurable: true, enumerable: false, writable: true
+		});
+		Object.defineProperty(this, 'headers', {
+			value: this._filterHeaders(headers), configurable: true, enumerable: false, writable: true
+		});
+	}
+
+	/**
+	 * @returns {boolean}
+	 */
+	get isClientError(){
+		return status.isClientErrorStatus(this.status);
+	}
+	
+	/**
+	 * @returns {boolean}
+	 */
+	get isServerError(){
+		return status.isServerErrorStatus(this.status);
+	}
+
+	/**
+	 * @param {number|void} status
+	 * @returns {number}
+	 */
+	_filterStatus(status){
+		if (status.isErrorStatus(status)) {
+			return status;
+		}
+		return 500;
+	}
+	
+	/**
+	 * @param {Object|void} headers
+	 * @returns {Object}
+	 */
+	_filterHeaders(headers){
+		if (Object.prototype.toString.call(headers) === '[object Object]') {
+			return headers;
+		}
+		return Object.create(null);
+	}
+}
+
+/**
+ * @+
+ */
+module.exports = Error;
---
+++ index.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C) 2019 Yudha Tama Aditiyara
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const http = require("http");
+const status = require("fifit-util-status");
+const Error = require("./error");
+const ClientError = require("./clientError");
+const ServerError = require("./serverError");
+
+/**
+ * @returns {Object}
+ */
+function setup(){
+	let name;
+	let constructor;
+	return status.entries.reduce((exports, entry) => {
+		let code = entry[0];
+		let message = entry[1];
+		if (status.isClientErrorStatus(code)) {
+			name = createClassName(status.identifiers[code]);
+			constructor = createClientErrorClass(name, code, message);
+			exports[name] = constructor;
+		} else if (status.isServerErrorStatus(code)) {
+			name = createClassName(status.identifiers[code]);
+			constructor = createServerErrorClass(name, code, message);
+			exports[name] = constructor;
+		}
+		return exports;
+	},{});
+}
+
+/**
+ * @param {string} name
+ * @returns {string}
+ */
+function createClassName(name){
+	if (!/Error$/.test(name)) {
+		name += "Error";
+	}
+	return name;
+}
+
+/**
+ * @param {string} name
+ * @param {number} statusCode
+ * @param {string} statusMessage
+ * @returns {function}
+ */
+function createClientErrorClass(name, statusCode, statusMessage){
+	return class extends ClientError{
+		constructor(message, headers){
+			super(statusCode, message != null ? message : statusMessage);
+			Object.defineProperty(this, "name", {
+				value: name, configurable: true, enumerable: false, writable: true
+			});
+		}
+	};
+}
+
+/**
+ * @param {string} name
+ * @param {number} statusCode
+ * @param {string} statusMessage
+ * @returns {function}
+ */
+function createServerErrorClass(name, statusCode, statusMessage){
+	return class extends ServerError{
+		constructor(message, headers){
+			super(statusCode, message != null ? message : statusMessage);
+			Object.defineProperty(this, "name", {
+				value: name, configurable: true, enumerable: false, writable: true
+			});
+		}
+	};
+}
+
+/**
+ * @+
+ */
+module.exports = setup();
+module.exports.Error = Error;
+module.exports.ClientError = ClientError;
+module.exports.ServerError = ServerError;
